69b4a78ced2ae5bcfb7de80019b5e06be167304b
NotDarkkkk <NotDarkFTW@gmail.com>
2025-02-05 05:29:30 +0100

    Seperated Header File

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <div id="Header">
-      <h1 class="left-text">Drawing Thing</h1>
-    </div>
+    <MainHeader />
     <HomeView id="GameCanvas" />
   </div>
 </template>
 
 <script lang="ts">
-// import GameCanvas from "./components/GameCanvas.vue";
+import MainHeader from "./components/MainHeader.vue";
 import HomeView from "./views/HomeView.vue";
 export default {
   name: "App",
   components: {
     HomeView,
+    MainHeader,
   },
 };
 </script>
@@ -23,35 +22,9 @@ html,
 body {
   margin: 0;
   padding: 0;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
-#Header {
   width: 100vw;
-  height: 7vh;
-  background-color: #3e3e59;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-  box-sizing: border-box;
-  /* border-bottom: 2px solid rgb(23, 128, 0); */
-  margin: 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 10;
-}
-
-#Header .left-text {
-  color: rgb(0, 0, 0);
-  font-size: 4vh;
-}
-
-#Header .right-text {
-  color: white;
-  font-size: 2vh;
+  height: 100vh;
+  overflow: hidden;
 }
 
 #app {
@@ -63,14 +36,6 @@ body {
   align-items: center;
   background-color: #5f5f78;
   /* border: 3px solid rgb(68, 156, 176); */
-  margin-top: 0; /* Ensure the content below the header starts at the top */
-}
-
-#GameCanvas {
-  background-color: rgb(255, 255, 255);
-  height: 80vh;
-  width: 100vh;
-  box-shadow: inset 2px black;
-  border: 3px solid rgb(61, 60, 73);
+  margin-top: 0;
 }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="Header">
+    <h1 class="left-text">Drawing Thing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+};
+</script>
+
+<style>
+#Header {
+  width: 100vw;
+  height: 7vh;
+  background-color: #3e3e59;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+  /* border-bottom: 2px solid rgb(23, 128, 0); */
+  margin: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  font-family: "Basier circle", -apple-system, system-ui, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+
+#Header .left-text {
+  color: rgb(0, 0, 0);
+  font-size: 4vh;
+}
+
+#Header .right-text {
+  color: white;
+  font-size: 2vh;
+}
+</style>
